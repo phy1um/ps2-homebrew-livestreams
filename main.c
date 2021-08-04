99a6973986836fe69766a6b4b25328af2b25ba9b
Tom Marks <tom@tommarks.xyz>
2021-08-05 06:06:09 +1000

    Cleanup

--- main.c
+++ main.c
@@ -15,7 +15,12 @@
 #define OFFSET_X 0
 #define OFFSET_Y 0
 
+#define VID_W 640
+#define VID_H 448
+
 static qword_t *buf; 
+
+#define DRAWBUF_LEN (100 * 16)
 
 int print_buffer(qword_t *b, int len)
 {
@@ -54,17 +59,14 @@ int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
   z.zsm = 0;
   z.mask = 0;
 
-  /*
   graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE); 
   graph_set_screen(0, 0, width, height);
   graph_set_bgcolor(0, 0, 0);
   graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
   graph_enable_output();
-  */
-  graph_initialize(fb.address, width, height, psm, 0, 0);
 
   qword_t *q = buf;
-  memset(buf, 0, sizeof(buf));
+  memset(buf, 0, DRAWBUF_LEN);
   q = draw_setup_environment(q, 0, &fb, &z);
   q = draw_finish(q);
   dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
@@ -123,21 +125,21 @@ qword_t *draw(qword_t *q)
 int main()
 {
   printf("Hello\n");
-  buf = malloc(16 * 100);
+  buf = malloc(DRAWBUF_LEN);
   // init DMAC
   dma_channel_initialize(DMA_CHANNEL_GIF, 0, 0);
   dma_channel_fast_waits(DMA_CHANNEL_GIF);
 
   int vmode = graph_get_region();
   // initialize graphics mode 
-  gs_init(512, 512, GS_PSM_32, GS_PSMZ_24, vmode, GRAPH_MODE_INTERLACED);
+  gs_init(VID_W, VID_H, GS_PSM_32, GS_PSMZ_24, vmode, GRAPH_MODE_INTERLACED);
   
   graph_wait_vsync();
   while(1) {
     dma_wait_fast();
     qword_t *q = buf;
-    memset(buf, 0, sizeof(buf));
-    q = draw_clear(q, 0, 0, 0, 512, 512, 20, 20, 20);
+    memset(buf, 0, DRAWBUF_LEN);
+    q = draw_clear(q, 0, 0, 0, VID_W, VID_H, 20, 20, 20);
     q = draw(q);
     q = draw_finish(q);
     dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
@@ -145,6 +147,7 @@ int main()
     draw_wait_finish();
     // wait for vsync
     graph_wait_vsync();
+    sleep(2);
   }
 }
 
