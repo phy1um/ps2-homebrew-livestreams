8bcb6f2054af039fc5c661b03a398d3f573076c7
Tom Marks <tom@tommarks.xyz>
2021-08-12 06:50:34 +1000

    Drawing and transforming in 3d

---
+++ draw.c
@@ -0,0 +1,50 @@
+
+#include <math3d.h>
+#include <draw_types.h>
+
+#include <stdint.h>
+
+#include "draw.h"
+#include "mesh.h"
+
+void mesh_transform(char *b, struct model_instance *inst, struct render_state *d)
+{
+  MATRIX tmp;
+  MATRIX model;
+  matrix_unit(model);
+  create_model_matrix(model, inst->translate, inst->scale, inst->rotate);
+  matrix_unit(tmp);
+  matrix_multiply(tmp, model, d->world_to_screen);
+  int stride = inst->m->vertex_size * 16;
+  for (int i = 0; i < inst->m->vertex_count; i++) {
+    // get address of current vertex data
+    float *pos = (float*) (b + (stride*i) + (inst->m->vertex_position_offset*16));
+    VECTOR *v = pos;
+    vector_apply(v, v, tmp);
+    *((uint32_t*)pos) = ftoi4(pos[0]+d->offset_x);
+    *((uint32_t*)(pos+1)) = ftoi4(pos[1]+d->offset_y);
+    *((uint32_t*)(pos+2)) = (int) pos[2];
+    /*
+    *((uint32_t*)pos) = (short)((pos[0]+1.0f)*d->offset_x);
+    *((uint32_t*)(pos+1)) = (short)((pos[1]+1.0f)*d->offset_y);
+    *((uint32_t*)(pos+2)) = (unsigned int)((pos[2]+1.0f)*20);
+    */
+
+    pos[3] = 0;
+  }
+}
+
+void create_model_matrix(MATRIX tgt, VECTOR translate, VECTOR scale, VECTOR rotate)
+{
+  matrix_unit(tgt);
+  matrix_rotate(tgt, tgt, rotate);
+  matrix_scale(tgt, tgt, scale);
+  matrix_translate(tgt, tgt, translate);
+}
+
+void update_draw_matrix(struct render_state *d)
+{
+  create_view_screen(d->view_screen, 3.0f/4.0f, -3.0f, 3.0f, -3.0f, 3.0f, 1.0f, 2000.0f);
+  create_world_view(d->world_view, d->camera_pos, d->camera_rot);
+  matrix_multiply(d->world_to_screen, d->world_view, d->view_screen);
+}
---
+++ draw.h
@@ -0,0 +1,31 @@
+
+#ifndef DRAW_H
+#define DRAW_H
+
+
+#define myftoi4(x) (((uint64_t)(x))<<4)
+
+struct render_state {
+  float offset_x;
+  float offset_y;
+  char clear_col[3];
+  MATRIX world_view;
+  MATRIX view_screen;
+  MATRIX world_to_screen;
+  VECTOR camera_pos;
+  VECTOR camera_rot;
+};
+
+struct model_instance {
+  struct model *m;
+  VECTOR translate;
+  VECTOR scale;
+  VECTOR rotate;
+};
+
+
+void mesh_transform(char *b, struct model_instance *inst, struct render_state *d);
+void create_model_matrix(MATRIX tgt, VECTOR translate, VECTOR scale, VECTOR rotate);
+void update_draw_matrix(struct render_state *d);
+
+#endif
