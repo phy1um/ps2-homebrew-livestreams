84b2af3134e3123a59370e2cb2e01b499cd0342f
Tom Marks <phylum@dlsym.so>
2021-08-30 19:14:49 +1000

    Add explicit byte order in asset pipeline (#5)
    * update makefiles to clean src and handle dist better
    
    * nuke dist folder on clean
    
    * explicit byte ordering in obj-to-bin conversion script
    
    Co-authored-by: Tom Marks <tom@tommarks.xyz>

--- asset/pipeline/conv.py
+++ asset/pipeline/conv.py
@@ -9,6 +9,11 @@ log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 log.addHandler(ch)
 
+def LE(s):
+    return f"<{s}"
+
+def BE(s):
+    return f">{s}"
 
 obj_file = open(sys.argv[1], "r")
 o = obj.parse(obj_file)
@@ -17,12 +22,12 @@ count = 0
 rows = []
 for f in o.face_coords():
     [a, b, c] = f
-    rows.append(struct.pack("ffff", 0, 0, 0, 0))
-    rows.append(struct.pack(">ffff", a.x, a.y, a.z, 1.0))
-    rows.append(struct.pack("ffff", 0, 0, 0, 0))
-    rows.append(struct.pack(">ffff", b.x, b.y, b.z, 1.0))
-    rows.append(struct.pack("ffff", 0, 0, 0, 0))
-    rows.append(struct.pack(">ffff", c.x, c.y, c.z, 1.0))
+    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    rows.append(struct.pack(LE("ffff"), a.x, a.y, a.z, 1.0))
+    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    rows.append(struct.pack(LE("ffff"), b.x, b.y, b.z, 1.0))
+    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    rows.append(struct.pack(LE("ffff"), c.x, c.y, c.z, 1.0))
     count += 1
 log.info(f"found {count} faces")
 obj_file.close()
