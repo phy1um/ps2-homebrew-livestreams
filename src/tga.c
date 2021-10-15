a44b794cfff2aa71227dca9a51d988852f6cdfcb
Tom Marks <tom@tommarks.xyz>
2021-10-16 00:43:23 +1100

    formatter

--- src/tga.c
+++ src/tga.c
@@ -1,10 +1,10 @@
+#include <lauxlib.h>
 #include <lua.h>
 #include <lualib.h>
-#include <lauxlib.h>
 
-#include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "log.h"
 
@@ -25,7 +25,7 @@ struct __attribute__((__packed__)) tga_header {
   uint8_t descriptor;
 };
 
-char tmp_buffer[256*256*4];
+char tmp_buffer[256 * 256 * 4];
 int load_tga_to_raw(const char *fname, void *buffer) {
   info("loading TGA %s", fname);
   FILE *f = fopen(fname, "rb");
@@ -37,16 +37,20 @@ int load_tga_to_raw(const char *fname, void *buffer) {
   rc = fread(idData, 1, header.idlen, f);
 
   // bytes per pixel from bits per pixel
-  int bpp = header.bps/8;
-  info("reading image data - %d bytes", header.width*header.height*bpp);
-  rc = fread(tmp_buffer, bpp, header.width*header.height*bpp, f);
-  char *to = (char *) buffer;
-  for ( int i = 0; i < header.width; i++ ) {
-    for ( int j = 0; j < header.height; j++ ) {
-      to[ (j*header.width + i) *4 + 3 ] = tmp_buffer[ (j*header.width + i) *4 + 3 ];
-      to[ (j*header.width + i) *4 + 2 ] = tmp_buffer[ (j*header.width + i) *4 + 0 ];
-      to[ (j*header.width + i) *4 + 1 ] = tmp_buffer[ (j*header.width + i) *4 + 1 ];
-      to[ (j*header.width + i) *4 + 0 ] = tmp_buffer[ (j*header.width + i) *4 + 2 ];
+  int bpp = header.bps / 8;
+  info("reading image data - %d bytes", header.width * header.height * bpp);
+  rc = fread(tmp_buffer, bpp, header.width * header.height * bpp, f);
+  char *to = (char *)buffer;
+  for (int i = 0; i < header.width; i++) {
+    for (int j = 0; j < header.height; j++) {
+      to[(j * header.width + i) * 4 + 3] =
+          tmp_buffer[(j * header.width + i) * 4 + 3];
+      to[(j * header.width + i) * 4 + 2] =
+          tmp_buffer[(j * header.width + i) * 4 + 0];
+      to[(j * header.width + i) * 4 + 1] =
+          tmp_buffer[(j * header.width + i) * 4 + 1];
+      to[(j * header.width + i) * 4 + 0] =
+          tmp_buffer[(j * header.width + i) * 4 + 2];
     }
   }
   fclose(f);
@@ -54,16 +58,16 @@ int load_tga_to_raw(const char *fname, void *buffer) {
 }
 
 int load_tga_lua(lua_State *l) {
-  const char *fname = lua_tostring(l, 1); 
+  const char *fname = lua_tostring(l, 1);
   int width = lua_tointeger(l, 2);
   int height = lua_tointeger(l, 3);
-  uint32_t *b = malloc(width*height*4);
+  uint32_t *b = malloc(width * height * 4);
   load_tga_to_raw(fname, b);
 
   lua_createtable(l, 0, 5);
   lua_pushinteger(l, 0);
   lua_setfield(l, -2, "head");
-  lua_pushinteger(l, width*height*4);
+  lua_pushinteger(l, width * height * 4);
   lua_setfield(l, -2, "size");
   lua_pushlightuserdata(l, b);
   lua_setfield(l, -2, "ptr");
