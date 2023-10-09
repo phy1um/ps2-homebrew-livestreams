5b1ef5741f0c8b8af1ffc223f06bc67cf219ad62
Tom Marks <tom@tommarks.xyz>
2023-10-09 21:59:14 +1100

    refined .obj pipeline, cube.obj re-added

--- asset/pipeline/conv.py
+++ asset/pipeline/conv.py
@@ -4,6 +4,9 @@ import logging
 
 import obj
 
+from random import random
+import math
+
 log = logging.Logger("obj2gif")
 log.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -15,6 +18,9 @@ def LE(s):
 def BE(s):
     return f">{s}"
 
+def random_col():
+    return (math.floor(random()*100) + 155, math.floor(random()*100) + 155, math.floor(random()*100) + 155)
+
 obj_file = open(sys.argv[1], "r")
 o = obj.parse(obj_file)
 obj_file.close()
@@ -22,11 +28,12 @@ count = 0
 rows = []
 for f in o.face_coords():
     [a, b, c] = f
-    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    (r, g, blu) = random_col()
+    rows.append(struct.pack(LE("iiii"), r, g, blu, 128))
     rows.append(struct.pack(LE("ffff"), a.x, a.y, a.z, 1.0))
-    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    rows.append(struct.pack(LE("iiii"), r, g, blu, 128))
     rows.append(struct.pack(LE("ffff"), b.x, b.y, b.z, 1.0))
-    rows.append(struct.pack(LE("ffff"), 0, 0, 0, 0))
+    rows.append(struct.pack(LE("iiii"), r, g, blu, 128))
     rows.append(struct.pack(LE("ffff"), c.x, c.y, c.z, 1.0))
     count += 1
 log.info(f"found {count} faces")
