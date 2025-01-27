abe324adf9466c3647ad158cac4649ddf0a80426
Tom Marks <tom@tommarks.xyz>
2025-01-27 11:42:06 +1100

    src/examples: vu1transform uses director/app API

--- src/examples/vu1transform.c
+++ src/examples/vu1transform.c
@@ -4,6 +4,7 @@
 #include <p2g/log.h>
 #include <p2g/ps2draw.h>
 #include <p2g/utils.h>
+#include <p2g/pad.h>
 
 // PS2SDK deps
 #include <dma.h>
@@ -22,44 +23,6 @@
 // vertex = 4x f32 XYZW, 4x u32 RGBA
 #define VERT_SIZE ((4*4) + (4*4))
 
-static void vu1_read_registers(qword_t regs[32]) {
-  register void * out asm("s1");
-  out = regs;
-  __asm__ __volatile__(
-    "li $s0, 1024 \n"
-    "sync \n"
-    "ctc2 $s0, $vi1 \n"
-    "li $a1, 32 \n"
-    "loop: \n"
-    "vlqi $vf1, ($vi1++) \n"
-    "sqc2 $vf1, 0(%[out]) \n"
-    "addiu %[out], %[out], 16 \n"
-    "addi $a1, $a1, -1 \n"
-    "bgez $a1, loop \n"
-    "sync.p \n"
-    : : [out]"r"(out) : "s0", "a0", "memory"
-  );
-}
-
-static void vu1_debug_dump(size_t memory_start, size_t memory_end) {
-  info("dumping vu1 data for debugging\n");
-  printf("# vu1 registers:\n");
-  qword_t registers[32];
-  registers[0].dw[0] = 0x98765432987654;
-  vu1_read_registers(registers);
-  for (int i = 0; i < 8; i++) {
-    printf("VF%02d: %016llX %016llX\n", i, registers[i].dw[1], registers[i].dw[0]);
-    printf("@ xyzw: %f %f %f %f\n", i, *((float*)&registers[i].sw[0]), *((float*)&registers[i].sw[1]), 
-      *((float*)&registers[i].sw[2]), *((float*)&registers[i].sw[3]));
-  }
-  printf("# vu1 umem:\n", memory_start, memory_end);
-  for (size_t i = memory_start; i < memory_end; i += 16) {
-    char *addr = (char *) (0x1100C000 + i);
-    uint32_t *addrw = (uint32_t *) addr;
-    printf("$%08X: %08lX %08lX %08lX %08lX\n", memory_start + i, addrw[3], addrw[2], addrw[1], addrw[0]);
-  }
-}
-
 static uint32_t VRAM_HEAD = 0;
 static uint32_t VRAM_SIZE = 4 * 1024 * 1024;
 uint32_t vram_alloc(uint32_t size, uint32_t align) {
@@ -75,31 +38,7 @@ uint32_t vram_alloc(uint32_t size, uint32_t align) {
   return out;
 }
 
-void enable_vu1_debug() {
-  __asm__ __volatile__(
-      "li $a0, 0xC00 \n"
-      "sync \n"
-      "ctc2 $a0, $28 \n"
-      "sync.p \n"
-      );
-}
-
-void vu1_resume() {
-  VIF1_FBRST = 8;
-}
-
-uint32_t read_vpu_stat() {
-  register uint32_t v1 asm("v1");
-  __asm__ __volatile__(
-    "sync.p \n" 
-    "cfc2 %[v1], $29 \n"
-    : [v1] "=r"(v1) : :
-  );
-  return v1;
-}
-
-
-int main(int argc, char *argv[]) {
+int appmain(int argc, char *argv[]) {
   log_output_level = LOG_LEVEL_TRACE;
 
   gs_init();
@@ -160,39 +99,43 @@ int main(int argc, char *argv[]) {
 
   int first_frame = 1;
 
-  enable_vu1_debug();
+  pad_init();
 
   while (1) {
     trace("START FRAME: %d", frame_count);
     draw_frame_start();
 
-    // read stat
-    uint32_t vpu_stat = read_vpu_stat();
-    trace("VPU stat = %X", vpu_stat);
-    if (vpu_stat & 0x400) {
-      trace("VPU1 stop @ T bit");
-      vu1_debug_dump(0, 0x100);
-      vu1_resume();
-    }
-
-
     if (first_frame) {
       draw_vu_upload_program(vu1prog_buffer, prog_file_size, 0x0, TARGET_VU1);      
       first_frame = 0;
     }
-    draw_vu_begin_unpack_verts(VIF_CODE_UNPACK_V432, 0x151515, 6, 0);
+    float mvp_matrix[16];
+    mvp_matrix[0] = 40.f;
+    mvp_matrix[1] = 40.f;
+    mvp_matrix[2] = 40.f;
+    draw_vu_begin_unpack_verts(VIF_CODE_UNPACK_V432, 0x151515, 6, 0, mvp_matrix, 16*sizeof(float));
     draw3d_mesh_triangles_cnt(buffer, vertex_count, VERT_SIZE);
     draw_vu_call_program(0x0);
     trace("FRAME END");
     draw_frame_end();
 
-    trace("WAIT FINISH");
-    draw_wait_finish();
-    trace("WAIT VSYNC: (%d)", frame_count);
-    graph_wait_vsync();
     trace("FLIP");
     gs_flip();
     frame_count += 1;
+    SleepThread();
 
   }
 }
+
+int main(int argc, char *argv[]) {
+  struct p2g_app app = {
+    .main = appmain,
+  };
+  if(p2g_app_init(&app)) {
+    p2g_fatal("startup error");
+  }
+  int rv = p2g_app_run(&app, argc, argv);
+  p2g_fatal("app main returned");
+}
+
+
