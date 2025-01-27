7803b620c61e43a5bed242c7576722441e1172a5
Tom Marks <tom@tommarks.xyz>
2025-01-27 11:40:49 +1100

    src/core.c: impl init and run app

--- src/core.c
+++ src/core.c
@@ -1,10 +1,29 @@
 
 #include <p2g/log.h>
 #include <p2g/core.h>
+#include "director.h"
 
 #ifndef LOG_LEVEL_DEFAULT
 #define LOG_LEVEL_DEFAULT LOG_LEVEL_DEBUG
 #endif
+
+int p2g_app_init(struct p2g_app *app) {
+  if (!app->main) {
+    logerr("NULL main function provided");
+    return 1;
+  }
+  return 0;
+}
+
+int p2g_app_run(struct p2g_app *app, int argc, char *argv[]) {
+  if (director_run()) {
+    p2g_fatal("director startup failed");
+    return 1;
+  }
+  int rv = app->main(argc, argv);
+  p2g_fatal("main fallthrough: return value=%d", rv);
+  return rv;
+}
 
 int log_output_level = LOG_LEVEL_DEFAULT;
 
@@ -18,7 +37,8 @@ void p2g_fatal(const char *msg, ...) {
 }
 
 void p2g_fatal_busyloop() {
-  while (1) {}
+  while (1) {
+  }
 }
 
 fatal_handler_t p2g_fatal_handler = p2g_fatal_busyloop;
--- src/include/p2g/core.h
+++ src/include/p2g/core.h
@@ -8,5 +8,13 @@ void p2g_fatal(const char *s, ...);
 extern fatal_handler_t p2g_fatal_handler;
 void p2g_set_fatal_handler(fatal_handler_t fn);
 
+typedef int(*p2g_app_main_fn)(int argc, char *argv[]);
+
+struct p2g_app {
+  p2g_app_main_fn main; 
+};
+
+int p2g_app_init(struct p2g_app *app);
+int p2g_app_run(struct p2g_app *app, int argc, char *argv[]);
 
 #endif
